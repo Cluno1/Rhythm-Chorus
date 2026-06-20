d9bed4cbcc6494282ae6da84a0bb1e9365a634f7
Anjishnu Nandi <anjicroma@gmail.com>
2026-06-20 16:59:42 +0530

    Fix workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         val overrideVersionCode = project.findProperty("versionCodeOverride")?.toString()?.toIntOrNull()
         val overrideVersionName = project.findProperty("versionNameOverride")?.toString()
         versionCode = overrideVersionCode ?: 514131080
-        versionName = overrideVersionName ?: "5.1.413.1080\ Beta"
+        versionName = overrideVersionName ?: "5.1.413.1080 Beta"
 
         val overrideReleaseDate = project.findProperty("releaseDateOverride")?.toString()
         buildConfigField("String", "RELEASE_DATE", "\"${overrideReleaseDate ?: "2026-06-20"}\"")
