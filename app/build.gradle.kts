aebd15e5d6bd3b641ad1733a335d866c249279b6
Anjishnu Nandi <anjicroma@gmail.com>
2026-08-25 11:44:59 +0530

    Release 5.4.464.1220 Nightly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,11 @@ android {
         
         val overrideVersionCode = project.findProperty("versionCodeOverride")?.toString()?.toIntOrNull()
         val overrideVersionName = project.findProperty("versionNameOverride")?.toString()
-        versionCode = overrideVersionCode ?: 544621216
-        versionName = overrideVersionName ?: "5.4.462.1216"
+        versionCode = overrideVersionCode ?: 544641220
+        versionName = overrideVersionName ?: "5.4.464.1220 Beta"
 
         val overrideReleaseDate = project.findProperty("releaseDateOverride")?.toString()
-        buildConfigField("String", "RELEASE_DATE", "\"${overrideReleaseDate ?: "2026-08-23"}\"")
+        buildConfigField("String", "RELEASE_DATE", "\"${overrideReleaseDate ?: "2026-08-25"}\"")
 
         val isNightly = project.findProperty("nightly")?.toString() == "true"
         buildConfigField("boolean", "IS_NIGHTLY", isNightly.toString())
